540ff2cc626fb05faae3641608054756d25fb832
n0364359 <michael.blydenburgh@libertymutual.com>
2019-02-15 20:59:07 -0500

    start recent movie display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import Styled from 'styled-components'
-import Nav from './components/Nav'
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Styled from 'styled-components';
+import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
+import RecentMovieList from './components/RecentMovieList';
+import TMDB_SECRET from './keys';
 
 class App extends Component {
+  state = {
+    "movies": [{"title":"test"},{"title":"test"},{"title":"test"}],
+  }
+
+  componentDidMount(){
+    console.log(TMDB_SECRET);
+  }
+
   render() {
     return (
       <Main>
         <Nav />
-        <Jumbotron title={"React(ion) Movies"} />
+        <Jumbotron title={"React(ion) Movies"} subtitle={"Powered by The Movie Database"} />
+        <Router>
+          <div>
+            <Route exact path='/' component={() => <RecentMovieList movies={this.state.movies} />} />
+            {/* <Route exact path='/search' component={} /> */}
+            {/* <Route exact path='/saved' component={} /> */}
+          </div>
+        </Router>
       </Main>
     );
   }
--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Styled from 'styled-components'
 
-const Jumbotron = ({title}) => {
+const Jumbotron = ({title,subtitle}) => {
     return (
         <JumboDiv>
             <h1>{title}</h1>
+            <h3>{subtitle}</h3>
         </JumboDiv>
     )
 }
@@ -14,4 +15,10 @@ export default Jumbotron;
 const JumboDiv = Styled.div`
     text-align: center;
     min-height: 300px;
+    padding: 1.25rem;
+    border: 1px solid black;
+    margin: 1rem;
+    display: grid;
+    justify-content: center;
+    align-content: center;
 `
