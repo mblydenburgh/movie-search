572a96d31af06b2a6a05c855a1172e9e67df9148
n0364359 <michael.blydenburgh@libertymutual.com>
2019-02-17 12:49:21 -0500

    movie detail component fetchs individual data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Styled from 'styled-components';
 import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
-import RecentMovieList from './components/RecentMovieList';
+import MovieList from './components/MovieList';
+import MovieDetail from './components/MovieList/MovieDetail';
 import API from './utils/api';
+import queryString from 'query-string';
 class App extends Component {
   state = {
     "movies": [],
@@ -23,9 +25,12 @@ class App extends Component {
         <Jumbotron title={"React(ion) Movies"} subtitle={"Powered by The Movie Database"} />
         <Router>
           <div>
-            <Route exact path='/' component={() => <RecentMovieList movies={this.state.movies} />} />
-            {/* <Route exact path='/search' component={} /> */}
-            {/* <Route exact path='/saved' component={} /> */}
+            <Switch>
+              <Route exact path='/' component={() => <MovieList movies={this.state.movies} />} />
+              <Route exact path='/:id' component={() => <MovieDetail />} />
+              {/* <Route exact path='/search' component={} /> */}
+              {/* <Route exact path='/saved' component={} /> */}
+            </Switch>
           </div>
         </Router>
       </Main>
